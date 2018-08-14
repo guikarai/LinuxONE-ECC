4cb446af49a9f32b44dcd4aa255056770874779e
Guillaume HOAREAU <guikarai@yahoo.fr>
2018-08-14 15:32:14 +0200

    Update signPDFwithRSA.sh

--- RSA/signPDFwithRSA.sh
+++ RSA/signPDFwithRSA.sh
@@ -1,7 +1,7 @@
 !/bin/bash
 echo "****************** SIGNATURE RSA Ciphers ****************************"
 for cipher in 1024 2048 3072 4096 7680 15360 ; do
-        echo "100 RSA Signature with $cipher"
+        echo "100x RSA Signature with $cipher"
 	time bash -c "for i in {1..100}; do  openssl dgst -sha256 -sign private-$cipher.pem test.pdf > signature-$cipher.bin;  done;"
         echo "End of Test for $cipher"
         echo ""

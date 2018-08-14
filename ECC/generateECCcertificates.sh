a6a592d4dd8ad0987f312e653ec4531b6835d3fe
root <root@ghinject.mop.fr.ibm.com>
2018-08-14 14:07:28 +0200

    updates about structure

---
+++ ECC/generateECCcertificates.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "****************** BEGIN OF ECC CERTIFICATE GENERATION  ****************************"
+for cipher in prime256v1 secp384r1 secp521r1 ; do        
+echo "****************** CERTIFICATE WITH $cipher  ****************************"
+        echo "ECC CERTIFICATE  with $cipher"
+	openssl ecparam -name $cipher -genkey -param_enc explicit -out private-$cipher.pem
+	openssl req -new -x509 -key private-$cipher.pem -out server-$cipher.pem -days 365 -sha256 -subj /C=FR/ST=France/L=Montpellier/CN=PSSC -engine ibmca 2>/dev/null
+	cat private-$cipher.pem server-$cipher.pem > mycert-$cipher.pem 
+	echo "End of Test for $cipher"
+        echo ""
+done;
+echo "******************END OF TEST ECC CERTIFICATE GENERATION  ****************************"
